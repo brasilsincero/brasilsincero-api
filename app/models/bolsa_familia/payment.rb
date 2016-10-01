8a4e2f1c996a04cf22c2337abed9ed17e7457a53
Thiago Guimaraes <thiagogsr@gmail.com>
2016-10-01 15:42:28 -0300

    Move partitionable setup to new class

--- app/models/bolsa_familia/payment.rb
+++ app/models/bolsa_familia/payment.rb
@@ -1,4 +1,15 @@
 module BolsaFamilia
   class Payment < ApplicationRecord
+    include Partitionable::ActsAsPartitionable
+
+    acts_as_partitionable index_fields:
+      %w(
+        uf
+        nome_municipio
+        nome_favorecido
+        valor_parcela
+        nis_favorecido
+        data_competencia
+      ), logdate_attr: 'data_competencia'
   end
 end
