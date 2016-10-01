d682ae88e1cbfdea85e032b1ed67af411d3a7784
Thiago Guimaraes <thiagogsr@gmail.com>
2016-10-01 16:41:46 -0300

    Decompress downloaded file

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'pg', '~> 0.18'
 gem 'puma', '~> 3.0'
 gem 'activerecord-import'
 gem 'partitionable'
+gem 'rubyzip'
 
 group :development, :test do
   gem 'byebug', platform: :mri
--- app/models/bolsa_familia/download.rb
+++ app/models/bolsa_familia/download.rb
@@ -1,4 +1,5 @@
 require 'net/http'
+require 'zip/zip'
 
 module BolsaFamilia
   class Download
@@ -7,6 +8,7 @@ module BolsaFamilia
       formatted_month = format('%02d', month.to_i)
       filename = "tmp/#{year}#{formatted_month}_BolsaFamiliaFolhaPagamento.zip"
       download(filename, formatted_month, year)
+      decompress(filename)
     end
 
     def self.download(filename, month, year)
@@ -21,6 +23,18 @@ module BolsaFamilia
       end
 
       Rails.logger.info "Finished download of #{filename}"
+    end
+
+    def self.decompress(filename)
+      dest_file = filename.gsub('zip', 'csv')
+      Zip::ZipFile.open(filename) do |zip_file|
+        zip_file.each do |entry|
+          Rails.logger.info "Extracting #{entry.name}"
+          entry.extract(dest_file)
+        end
+      end
+      Rails.logger.info "Removing #{filename}"
+      FileUtils.rm(filename)
     end
   end
 end
