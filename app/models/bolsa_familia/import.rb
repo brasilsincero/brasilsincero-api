1d4a222a979370d2fa87a4797b02ad65f9bcae69
Thiago Guimaraes <thiagogsr@gmail.com>
2016-10-01 17:59:38 -0300

    Change rails log do stdout

--- app/models/bolsa_familia/import.rb
+++ app/models/bolsa_familia/import.rb
@@ -1,6 +1,7 @@
 module BolsaFamilia
   class Import
     def self.perform(month, year)
+      Rails.logger = Logger.new(STDOUT)
       filename = BolsaFamilia.generate_filename(month, year)
       Rails.logger.info "Importing #{filename}"
       system "path_prefix='#{filename}' embulk run lib/embulk/bolsa_familia.yml.liquid"
