85fad77bb83fdaeb430b431858fc6fa8c1de6178
Thiago Guimaraes <thiagogsr@gmail.com>
2016-11-17 00:28:02 -0300

    Add dependency to environment

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,7 +42,7 @@ task :setup do
 end
 
 desc "Deploys the current version to the server."
-task :deploy do
+task deploy: :environment do
   # uncomment this line to make sure you pushed your local branch to the remote origin
   # invoke :'git:ensure_pushed'
   deploy do
