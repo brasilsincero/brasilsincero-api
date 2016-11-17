a88a3596990f63f84db29a33581d7fd21b489c6e
Thiago Guimaraes <thiagogsr@gmail.com>
2016-11-16 23:45:05 -0300

    Remove assets precompile from deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,7 +51,6 @@ task :deploy do
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
-    invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
     on :launch do
